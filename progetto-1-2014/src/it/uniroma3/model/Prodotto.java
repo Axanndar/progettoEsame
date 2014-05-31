fcb77af69f84adb999fe389b7e93633cfd0d3314
andreaalessi <andreaalessi@fastwebnet.it>
2014-05-31 15:17:03 +0200

    commit

--- progetto-1-2014/src/it/uniroma3/model/Prodotto.java
+++ progetto-1-2014/src/it/uniroma3/model/Prodotto.java
@@ -3,78 +3,76 @@ package it.uniroma3.model;
 import java.util.List;
 import javax.persistence.*;
 
+@Entity
+@NamedQuery(name = "findAllProdotti", query = "SELECT p FROM Prodotto p")
+public class Prodotto {
 
-	@Entity
-	@NamedQuery(name = "findAllProdotti", query = "SELECT p FROM Prodotto p")
-	public class Prodotto {
-        
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long  id;
+	private Long id;
 
 	@Column(nullable = false)
 	private String nome;
 
 	private Float prezzo;
-	
+
 	@Column(length = 2000)
-    private String descrizione;
+	private String descrizione;
 
 	@Column(nullable = false)
 	private String codice;
-	
-	@ManyToMany(mappedBy= "prodotti")
+
+	@ManyToMany(mappedBy = "prodotti")
 	private List<Fornitore> fornitori;
-	
-	
+
 	public Prodotto() {
-    }
+	}
 
 	public Prodotto(String nome, Float prezzo, String descrizione, String codice) {
-        this.nome = nome;
-        this.prezzo = prezzo;
-        this.descrizione = descrizione;
-        this.codice = codice;
-}
+		this.nome = nome;
+		this.prezzo = prezzo;
+		this.descrizione = descrizione;
+		this.codice = codice;
+	}
 
-    //          Getters & Setters        
-    
-   public Long getId() {
-        return id;
-    }
+	// Getters & Setters
 
-    public String getNome() {
-        return this.nome;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    
-    public String getCodice() {
-        return this.codice;
-    }
+	public String getNome() {
+		return this.nome;
+	}
 
-    public void setCodice(String codice) {
-        this.codice = codice;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public String getDescrizione() {
-        return this.descrizione;
-    }
+	public String getCodice() {
+		return this.codice;
+	}
 
-    public void setDescrizione(String descrizione) {
-        this.descrizione = descrizione;
-    }
-    
-    public Float getPrezzo() {
-        return prezzo;
-    }
+	public void setCodice(String codice) {
+		this.codice = codice;
+	}
 
-    public void setPrezzo(Float prezzo) {
-        this.prezzo = prezzo;
-    }
-	
+	public String getDescrizione() {
+		return this.descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public Float getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(Float prezzo) {
+		this.prezzo = prezzo;
+	}
+
 	public boolean equals(Object obj) {
 		Prodotto prodotto = (Prodotto) obj;
 		return this.getCodice().equals(prodotto.getCodice());
@@ -87,5 +85,5 @@ import javax.persistence.*;
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 }
